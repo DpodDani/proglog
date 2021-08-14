1b8a6a051d27fac6005d7e521d1b5985f01fe806
Daniel Namu-Fetha <d.namu.fetha@gmail.com>
2021-08-14 14:52:51 +0100

    impl. NewIndex() and Close()

---
+++ internal/log/index.go
@@ -0,0 +1,76 @@
+package log
+
+import (
+	"os"
+
+	"github.com/tysontate/gommap"
+)
+
+var (
+	offWidth uint64 = 4
+	posWidth uint64 = 8
+	entWidth        = offWidth + posWidth
+)
+
+type index struct {
+	file *os.File
+	mmap gommap.MMap
+	size uint64
+}
+
+func NewIndex(f *os.File, c Config) (*index, error) {
+	idx := &index{
+		file: f,
+	}
+
+	fi, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, err
+	}
+
+	idx.size = uint64(fi.Size())
+	// Truncate() either removes excess data from the file,
+	// or fills it with null characters so that file size matches
+	// value in 2nd argument.
+	//
+	// This is because the file cannot grow after it has been memory-mapped,
+	// so better to do it at the beginning.
+	//
+	// This means the last index entry is not necessarily at the end of the file
+	if err = os.Truncate(
+		f.Name(), int64(c.Segment.MaxIndexBytes),
+	); err != nil {
+		return nil, err
+	}
+
+	if idx.mmap, err = gommap.Map(
+		idx.file.Fd(),
+		gommap.PROT_READ|gommap.PROT_WRITE,
+		gommap.MAP_SHARED,
+	); err != nil {
+		return nil, err
+	}
+
+	return idx, nil
+}
+
+func (i *index) Close() error {
+	if err := i.mmap.Sync(gommap.MS_SYNC); err != nil {
+		return err
+	}
+
+	if err := i.file.Sync(); err != nil {
+		return err
+	}
+
+	// Truncate() is called again to get rid of any excess null characters,
+	// which were potentially introduced by Truncate() in index constructor.
+	//
+	// This enables the service to find the last index entry and determine
+	// the next offset.
+	if err = i.file.Truncate(int64(i.size)); err != nil {
+		return err
+	}
+
+	return i.file.Close()
+}
