6febd756d0d9ff3fd187022f95a411e43ecb1b9f
Daniel Namu-Fetha <d.namu.fetha@gmail.com>
2021-08-14 17:17:45 +0100

    test: add tests for index

---
+++ internal/log/index_test.go
@@ -0,0 +1,57 @@
+package log
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestIndex(t *testing.T) {
+	f, err := ioutil.TempFile(os.TempDir(), "index_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+
+	var c Config = Config{}
+	c.Segment.MaxIndexBytes = 1024
+	idx, err := NewIndex(f, c)
+	require.NoError(t, err)
+
+	// check that io.EOF is returned when reading empty index
+	_, _, err = idx.Read(-1)
+	require.Error(t, err)
+	require.Equal(t, f.Name(), idx.Name())
+	require.Equal(t, io.EOF, err)
+
+	entries := []struct {
+		Off uint32
+		Pos uint64
+	}{
+		{Off: 0, Pos: 0},
+		{Off: 1, Pos: 10},
+	}
+
+	for _, want := range entries {
+		err = idx.Write(want.Off, want.Pos)
+		require.NoError(t, err)
+
+		_, pos, err := idx.Read(int64(want.Off))
+		require.NoError(t, err)
+		require.Equal(t, pos, want.Pos)
+	}
+
+	// index and scanner should error when reading past existing records
+	_, _, err = idx.Read(int64(len(entries)))
+	require.Equal(t, io.EOF, err)
+	_ = idx.Close()
+
+	// index should build its state from existing file
+	f, _ = os.OpenFile(f.Name(), os.O_RDWR, 0600)
+	idx, err = NewIndex(f, c)
+	require.NoError(t, err)
+	off, pos, err := idx.Read(-1)
+	require.Equal(t, off, entries[1].Off)
+	require.Equal(t, pos, entries[1].Pos)
+}
