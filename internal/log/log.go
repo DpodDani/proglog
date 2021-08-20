1a3cadec55b588c20b97dfa93541fc5da65b9196
Daniel Namu-Fetha <d.namu.fetha@gmail.com>
2021-08-20 12:39:27 +0100

    doc(log): add comments in setup()

--- internal/log/log.go
+++ internal/log/log.go
@@ -34,6 +34,7 @@ func NewLog(dir string, c Config) (*Log, error) {
 }
 
 func (l *Log) setup() error {
+	// fetch all the index and store files
 	files, err := ioutil.ReadDir(l.Dir)
 	if err != nil {
 		return err
@@ -41,18 +42,24 @@ func (l *Log) setup() error {
 
 	var baseOffsets []uint64
 	for _, file := range files {
+		// obtain offset (string) number from fetched index and store files
+		// note: we'll have duplicate offset numbers - one from each pair of
+		// index and store files
 		offStr := strings.TrimSuffix(
 			file.Name(),
 			path.Ext(file.Name()),
 		)
+		// converts offStr (decimal base) into an uint64 value
 		off, _ := strconv.ParseUint(offStr, 10, 0)
 		baseOffsets = append(baseOffsets, off)
 	}
 
+	// sort array of offsets in ascending order
 	sort.Slice(baseOffsets, func(i, j int) bool {
 		return baseOffsets[i] < baseOffsets[j]
 	})
 
+	// TODO: why not use i+=2 instead?
 	for i := 0; i < len(baseOffsets); i++ {
 		if err = l.newSegment(baseOffset[i]); err != nil {
 			return err
@@ -64,7 +71,7 @@ func (l *Log) setup() error {
 
 	if l.segments == nil {
 		if err = l.newSegment(
-			l.Config.Segment.InitialOffset,
+			l.Config.Segment.InitialOffset, // zero value is 0
 		); err != nil {
 			return err
 		}
