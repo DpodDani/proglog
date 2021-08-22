c82b1e424a7db736ea76990f90232b505fb12f47
Daniel Namu-Fetha <d.namu.fetha@gmail.com>
2021-08-22 15:19:09 +0100

    feat(gRPC server): impl. Consume()

--- internal/server/server.go
+++ internal/server/server.go
@@ -44,3 +44,12 @@ func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (
 	}
 	return &api.ProduceResponse{Offset: offset}, nil
 }
+
+func (s *grpcServer) Consume(ctx context.Context, req *api.ConsumeRequest) (
+	*api.ConsumeResponse, error) {
+	record, err := s.CommitLog.Read(req.Offset)
+	if err != nil {
+		return nil, err
+	}
+	return &api.ConsumeResponse{Record: record}, nil
+}
