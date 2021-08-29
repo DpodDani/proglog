c8a544af39296f8258098a1ce48cec9fa978c823
Daniel Namu-Fetha <d.namu.fetha@gmail.com>
2021-08-29 13:35:11 +0100

    feat: impl. authoriser using Casbin library

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/DpodDani/proglog
 go 1.16
 
 require (
+	github.com/casbin/casbin v1.9.1 // indirect
 	github.com/cloudflare/cfssl v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
---
+++ internal/auth/authorizer.go
@@ -0,0 +1,39 @@
+package auth
+
+import (
+	"fmt"
+
+	"github.com/casbin/casbin"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+// model --> filepath to where models are defined
+//	--> configures Casbin's authorisation mechanism (in our case it's ACL)
+// policy --> filepath to where policies are defined
+func New(model, policy string) *Authorizer {
+	// casbin supports enforcing authorisation based on various control models
+	// we'll be using Access Control Lists (ACLs) in this project
+	enforcer := casbin.NewEnforcer(model, policy)
+	return &Authorizer{
+		enforcer: enforcer,
+	}
+}
+
+type Authorizer struct {
+	enforcer *casbin.Enforcer
+}
+
+func (a *Authorizer) Authorize(subject, object, action string) error {
+	if !a.enforcer.Enforce(subject, object, action) {
+		msg := fmt.Sprintf(
+			"%s not permitted to %s to %s",
+			subject,
+			action,
+			object,
+		)
+		st := status.New(codes.PermissionDenied, msg)
+		return st.Err()
+	}
+	return nil
+}
