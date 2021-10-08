d1243f05b1dac1d1a12fc8358baf0c30b09041a3
Daniel Namu-Fetha <d.namu.fetha@gmail.com>
2021-10-08 19:41:00 +0100

    feat(distributed.go): expose Raft server data via GetServers()

--- internal/log/distributed.go
+++ internal/log/distributed.go
@@ -297,6 +297,23 @@ func (l *DistributedLog) Close() error {
 	return l.log.Close()
 }
 
+func (l *DistributedLog) GetServers() ([]*api.Server, error) {
+	future := l.raft.GetConfiguration()
+	if err := future.Error(); err != nil {
+		return nil, err
+	}
+
+	var servers []*api.Server
+	for _, server := range future.Configuration().Servers {
+		servers = append(servers, &api.Server{
+			Id:       string(server.ID),
+			RpcAddr:  string(server.Address),
+			IsLeader: l.raft.Leader() == server.Address,
+		})
+	}
+	return servers, nil
+}
+
 // provides static (compile-time) check that our fsm struct satisfies the
 // raft.FSM interface
 var _ raft.FSM = (*fsm)(nil)
