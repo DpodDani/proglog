2793a33254cda4a81dc2e3a9cb8ba737659c3d9a
Daniel Namu-Fetha <d.namu.fetha@gmail.com>
2021-10-27 19:46:08 +0100

    impl. gRPC's resolver.Resolver interface in our resolver

--- internal/loadbalance/resolver.go
+++ internal/loadbalance/resolver.go
@@ -1,13 +1,17 @@
 package loadbalance
 
 import (
+	"context"
 	"fmt"
 	"sync"
 
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/attributes"
 	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/serviceconfig"
+
+	api "github.com/DpodDani/proglog/api/v1"
 )
 
 // resolver is used by gRPC to discover servers
@@ -47,6 +51,9 @@ func (r *Resolver) Build(
 		)
 	}
 
+	// indicate that we want to use the proglog load balancer (which we will
+	// implement a little later).
+	//
 	// raw string literals delimited by backticks are interpreted literally
 	// I guess this is needed because ParseServiceConfig takes in a JSON string
 	// ref: https://yourbasic.org/golang/multiline-string/
@@ -63,8 +70,6 @@ func (r *Resolver) Build(
 	r.ResolveNow(resolver.ResolveNowOptions{})
 
 	return r, nil
-
-	return nil, nil
 }
 
 const Name = "proglog"
@@ -83,4 +88,64 @@ func (r *Resolver) Scheme() string {
 // it's looking for resolvers that match the target scheme
 func init() {
 	resolver.Register(&Resolver{})
+}
+
+// compile-time check to make sure our Resolver satisfies the resolver.Resolver
+// interface
+var _ resolver.Resolver = (*Resolver)(nil)
+
+// gRPC calls this function to resolve the target, discover the servers and
+// update the client connection with the discovered servers
+//
+// this function can be called concurrently so we use mutexes to protect
+// access across goroutines
+func (r *Resolver) ResolveNow(resolver.ResolveNowOptions) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	client := api.NewLogClient(r.resolverConn)
+	// get cluster and then set on cc attributes
+	ctx := context.Background()
+	res, err := client.GetServers(ctx, &api.GetServersRequest{})
+	if err != nil {
+		r.logger.Error(
+			"failed to resolve server",
+			zap.Error(err),
+		)
+	}
+
+	var addrs []resolver.Address
+	for _, server := range res.Servers {
+		addrs = append(addrs, resolver.Address{
+			Addr: server.RpcAddr,
+			Attributes: attributes.New(
+				"is_leader",
+				server.IsLeader,
+			),
+		})
+	}
+
+	// update the state with slice of addresses to inform load balancer what
+	// servers it can choose from.
+	//
+	// resolver.Address has 3 fields:
+	// 	1) Addr --> address of the server to connect to.
+	//	2) Attributes --> map containing any data that's useful for load
+	//	   balancer. We use it to indicate whether a server is a leader or not.
+	//	3) ServerName --> name used as the transport certificate authority
+	//	   for the address, instead of the hostname taken from the Dial()
+	//	   target string.
+	r.clientConn.UpdateState(resolver.State{
+		Addresses:     addrs,
+		ServiceConfig: r.serviceConfig,
+	})
+}
+
+func (r *Resolver) Close() {
+	if err := r.resolverConn.Close(); err != nil {
+		r.logger.Error(
+			"failed to close conn",
+			zap.Error(err),
+		)
+	}
 }
