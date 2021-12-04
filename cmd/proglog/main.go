913428c0f8ef6c077e9b285ea9022e059d9b871d
Daniel Namu-Fetha <d.namu.fetha@gmail.com>
2021-12-04 19:31:07 +0000

    impl. run() for cli struct

--- cmd/proglog/main.go
+++ cmd/proglog/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"log"
 	"os"
+	"os/signal"
 	"path"
+	"syscall"
 
 	"github.com/DpodDani/proglog/internal/agent"
 	"github.com/DpodDani/proglog/internal/config"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 func main() {
@@ -16,7 +19,7 @@ func main() {
 	// defines the sole command
 	cmd := &cobra.Command{
 		Use:     "proglog",
-		PreRunE: cli.setupConfig, // hook function
+		PreRunE: cli.setupConfig, // hook function called before RunE
 		// contains primary logic
 		// calls this function when command runs
 		RunE: cli.run,
@@ -87,4 +90,80 @@ func setupFlags(cmd *cobra.Command) error {
 		"Path to peer certificate authority")
 
 	return viper.BindFlags(cmd.Flags())
+}
+
+// reads configuration and prepares the agent's configuration
+func (c *cli) setupConfig(cmd *cobra.Command, args []string) error {
+	var err error
+
+	configFile, err := cmd.Flags().GetString("config-file")
+	if err != nil {
+		return err
+	}
+	// Viper provides centralised config registry system, such that multiple
+	// configuration sources (eg. file, CLI flags etc.) can set the
+	// configuration BUT everything is handled (read) in one place!
+	viper.SetConfigFile(configFile)
+
+	if err = viper.ReadInConfig(); err != nil {
+		// it's okay if config file doesn't exist
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return err
+		}
+	}
+
+	c.cfg.DataDir = viper.GetString("data-dir")
+	c.cfg.NodeName = viper.GetString("node-name")
+	c.cfg.BindAddr = viper.GetString("bind-addr")
+	c.cfg.RPCPort = viper.GetInt("rpc-port")
+	c.cfg.StartJoinAddrs = viper.GetStringSlice("start-join-addrs")
+	c.cfg.Bootstrap = viper.GetBool("bootstrap")
+	c.cfg.ACLModelFile = viper.GetString("acl-model-file")
+
+	c.cfg.ServerTLSConfig.CertFile = viper.GetString("server-tls-cert-file")
+	c.cfg.ServerTLSConfig.KeyFile = viper.GetString("server-tls-key-file")
+	c.cfg.ServerTLSConfig.CAFile = viper.GetString("server-tls-ca-file")
+
+	c.cfg.PeerTLSConfig.CertFile = viper.GetString("peer-tls-cert-file")
+	c.cfg.PeerTLSConfig.KeyFile = viper.GetString("peer-tls-key-file")
+	c.cfg.PeerTLSConfig.CAFile = viper.GetString("peer-tls-ca-file")
+
+	if c.cfg.ServerTLSConfig.CertFile != "" &&
+		c.cfg.ServerTLSConfig.KeyFile != "" {
+		c.cfg.ServerTLSConfig.Server = true
+		c.cfg.Config.ServerTLSConfig, err = config.SetupTLSConfig(
+			c.cfg.ServerTLSConfig,
+		)
+		if err != nil {
+			return err
+		}
+	}
+
+	if c.cfg.PeerTLSConfig.CertFile != "" &&
+		c.cfg.PeerTLSConfig.KeyFile != "" {
+		c.cfg.Config.PeerTLSConfig, err = config.SetupTLSConfig(
+			c.cfg.PeerTLSConfig,
+		)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (c *cli) run(cmd *cobra.Command, args []string) error {
+	var err error
+	// create agent
+	agent, err := agent.New(c.cfg.Config)
+	if err != nil {
+		return err
+	}
+
+	sigc := make(chan os.Signal, 1)
+	// handle signals from operating system
+	signal.Notify(sigc, syscall.SIGINT, syscall.SIGTERM)
+	<-sigc
+	// shut down agent gracefully when operating system terminates program
+	agent.ShutDown()
 }
--- go.mod
+++ go.mod
@@ -10,19 +10,18 @@ require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0 // indirect
-	github.com/hashicorp/go-hclog v0.9.1 // indirect
 	github.com/hashicorp/go-msgpack v0.5.5 // indirect
 	github.com/hashicorp/raft v1.3.1 // indirect
 	github.com/hashicorp/raft-boltdb v0.0.0-20210422161416-485fa74b0b01 // indirect
 	github.com/hashicorp/serf v0.9.5 // indirect
 	github.com/soheilhy/cmux v0.1.5 // indirect
 	github.com/spf13/cobra v1.2.1 // indirect
+	github.com/spf13/viper v1.9.0 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/travisjeffery/go-dynaport v1.0.0 // indirect
 	github.com/tysontate/gommap v0.0.0-20210506040252-ef38c88b18e1 // indirect
 	go.opencensus.io v0.23.0 // indirect
 	go.uber.org/zap v1.19.0 // indirect
-	google.golang.org/genproto v0.0.0-20210825212027-de86158e7fda // indirect
 	google.golang.org/grpc v1.40.0 // indirect
 	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
